7eb5d9e25cd9648791f4c4c5fc923c6d8ed50758
nihuajun <397774208@qq.com>
2020-01-13 19:11:07 +0800

    1.集成好百川sdk，并初始化成功

--- app/src/main/java/com/zww/youquan/MyApp.java
+++ app/src/main/java/com/zww/youquan/MyApp.java
@@ -1,7 +1,13 @@
 package com.zww.youquan;
 
 
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.multidex.MultiDexApplication;
+
+import com.alibaba.baichuan.android.trade.AlibcTradeSDK;
+import com.alibaba.baichuan.android.trade.callback.AlibcTradeInitCallback;
 
 public class MyApp extends MultiDexApplication {
 
@@ -14,6 +20,24 @@ public class MyApp extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
         myApp=this;
+        initBaiChuanSDK();
+    }
+
+
+    private void initBaiChuanSDK(){
+        AlibcTradeSDK.asyncInit(this, new AlibcTradeInitCallback() {
+            @Override
+            public void onSuccess() {
+                Log.d("MyApp","初始化成功");
+                Toast.makeText(MyApp.this, "初始化成功", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int code, String msg) {
+                Log.d("MyApp","初始化失败:"+"code---->"+code+",msg--->"+msg);
+                Toast.makeText(MyApp.this, "初始化失败", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
